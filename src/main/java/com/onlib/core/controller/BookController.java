5b6b771daa80a7fe8d0ab40be8d02c95d3e2527c
KrakenGog <takun.lesha.unity@mail.ru>
2024-11-23 10:57:52 +0300

    Integrade epub reading lib

--- src/main/java/com/onlib/core/controller/BookController.java
+++ src/main/java/com/onlib/core/controller/BookController.java
@@ -9,22 +9,28 @@ import com.onlib.core.util.SimpleStringSearcher;
 
 import io.micrometer.core.instrument.util.IOUtils;
 
-
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 import org.springframework.http.ContentDisposition;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 
 @RestController
 public class BookController {
@@ -48,16 +54,29 @@ public class BookController {
             throw new RuntimeException("Cant find book with id: " + id);
     }
 
-    @GetMapping("/getBookEpubFile")
+    // @GetMapping("/getBookEpubFile")
+    // public ResponseEntity<Resource> getMethodName(@RequestParam long id) throws
+    // RuntimeException, IOException {
+    // Book book = getBook(id);
+    //
+    // Resource file = new ClassPathResource("static/books/" +
+    // book.getEpubFileName());
+    // return ResponseEntity.ok()
+    // .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;
+    // filename=\"file.epub\"")
+    // .contentType(MediaType.APPLICATION_OCTET_STREAM)
+    // .body(file);
+    // }
+    @GetMapping("/getBookEpubFile.epub")
     public ResponseEntity<byte[]> getMethodName(@RequestParam long id) throws RuntimeException, IOException {
         Book book = getBook(id);
-        ClassPathResource resource = new ClassPathResource("static/books/" + book.getEpubFileName());
-        InputStream in = resource.getInputStream();
+        File file = ResourceUtils.getFile("classpath:static/books/" + book.getEpubFileName());
+        InputStream in = new FileInputStream(file);
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + id + ".epub");
-        headers.add(HttpHeaders.CONTENT_TYPE, "application/epub+zip");
-        
-        return new ResponseEntity<>(in.readAllBytes(), headers, HttpStatus.OK);
+        headers.setContentType(MediaType.valueOf("application/epub+zip"));
+        byte[] bytes = in.readAllBytes();
+        in.close();
+        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
     }
 
 }
