a697a948121d9e59996f03c2ae083f695c37da1c
SanchukS <SanchukS2005@gmail.com>
2024-12-02 22:21:50 +0300

    Add ReviewService, realize addReview method

---
+++ src/main/java/com/onlib/core/service/ReviewService.java
@@ -0,0 +1,24 @@
+package com.onlib.core.service;
+
+import com.onlib.core.model.Book;
+import com.onlib.core.model.Review;
+import com.onlib.core.model.User;
+import com.onlib.core.repository.ReviewRepository;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Transactional
+    public void addReview(String text, User user, Book book) {
+        Review review = new Review(text);
+        review.setUser(user);
+        review.setBook(book);
+        reviewRepository.save(review);
+    }
+}
