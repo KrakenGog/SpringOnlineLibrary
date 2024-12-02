5a841e24e44b5ab6c9b716a76999c21376baa83a
SanchukS <SanchukS2005@gmail.com>
2024-12-02 22:21:08 +0300

    Add ReviewController, realize getBookReviews, addBookReview

---
+++ src/main/java/com/onlib/core/controller/ReviewController.java
@@ -0,0 +1,38 @@
+package com.onlib.core.controller;
+
+import com.onlib.core.dto.ReviewWithoutBookDto;
+import com.onlib.core.model.Book;
+import com.onlib.core.model.User;
+import com.onlib.core.repository.BookRepository;
+import com.onlib.core.repository.ReviewRepository;
+import com.onlib.core.repository.UserRepository;
+import com.onlib.core.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ReviewController {
+    @Autowired
+    ReviewService reviewService;
+
+    @GetMapping("/getBookReviews")
+    public List<ReviewWithoutBookDto> getBookReviews(@RequestParam Book book) {
+        return book.getReviews().stream()
+                .map(ReviewWithoutBookDto::new)
+                .toList();
+    }
+
+    @GetMapping("/addBookReview")
+    public void addBookReview(
+            @RequestParam Book book,
+            @RequestParam User user,
+            @RequestParam String reviewText
+    ) {
+        reviewService.addReview(reviewText, user, book);
+    }
+}
