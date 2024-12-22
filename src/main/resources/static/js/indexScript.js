94fc9833c55b4b56e9a314c6fec1c3e8586230b1
SanchukS <SanchukS2005@gmail.com>
2024-12-22 23:20:31 +0300

    indexScript.js requires debug
    fix some bugs. Now model-window isn't shown

--- src/main/resources/static/js/indexScript.js
+++ src/main/resources/static/js/indexScript.js
@@ -1,11 +1,20 @@
 const form = document.getElementById('search-form')
 const resultsDiv = document.getElementById('search-form__results');
+const modalWindow = document.getElementById("modal-window");
+const contentPlace = document.getElementById("content-place");
 
-form.addEventListener('input', async (event) => {
-    event.preventDefault(); // Отменяем стандартное поведение формы
+form.oninput = (event) => searchBooks(event);
+
+async function searchBooks(event){
+    event.preventDefault();
 
     const formData = new FormData(form);
     const query = formData.get('query');
+
+    if (typeof query != 'string') {
+        resultsDiv.innerHTML = '<p>Книги не найдены.</p>';
+        return;
+    }
 
     // Отправка запроса на сервер
     const response = await fetch(`/searchBooks?query=${encodeURIComponent(query)}`);
@@ -16,117 +25,123 @@ form.addEventListener('input', async (event) => {
 
     // Отображение результатов
     if (data.length > 0) {
-        data.forEach(book => {
-            // Отображение результатов
-            const bookDiv = document.createElement('div');
-            bookDiv.className = 'book-div';
-            const authorsJS = book.authors;
-            const authorNames = authorsJS.map(authors => authors.name);
-            const authorsStr = authorNames.join(', ')
-            bookDiv.innerHTML = `<p> <strong>${book.name}</strong> (${authorsStr})</p> `;
-
-            // Добавление кнопки для перехода к описанию книги
-            const readButton = document.createElement('button');
-            readButton.textContent = 'читать';
-            readButton.setAttribute("bookId", book.id);
-            
-            // Обработка события нажатия на кнопку
-            readButton.onclick = async function (event) {
-                event.preventDefault();
-                // Запрашиваем информацию о книге у сервера
-                const bookId = readButton.getAttribute("bookId");
-                const bookInfoResponse = await fetch(`/getBookInfo?id=${encodeURIComponent(bookId)}`);
-                const bookInfo = await bookInfoResponse.json();
-
-                // Определяем модальное окно и его содержимое
-                const modalWindow = document.getElementById("modal-window");
-                const contentPlace = document.getElementById("content-place");
-
-
-                // Загружаем шаблон book-info-content
-                const response = await fetch("book-info-content.html");
-                contentPlace.innerHTML = await response.text();
-                const bookInfoContent = contentPlace.getById("book-info-content");
-
-                // Выделяем нужные поля в шаблоне
-                const nameHtml = bookInfoContent.getElementById("name");
-                const authorsDivHtml = bookInfoContent.getElementById("authors");
-                const descriptionHtml = bookInfoContent.getElementById("description");
-                const reviewsDivHtml = bookInfoContent.getElementById("reviews");
-                const readButton = bookInfoContent.getElementById("read-button");
-
-                // Обновление содержимого модального окна
-                // Название книги
-                nameHtml.textContent = bookInfo.name;
-                // Список авторов
-                bookInfo.authors.forEach(author => {
-                    const authorP = document.createElement('p');
-                    authorP.className = "book-info-content__text";
-
-                    authorP.textContent = author.name;
-
-                    // На будущее сохранил id автора
-                    authorP.setAttribute("authorId", author.id);
-
-                    authorsDivHtml.append(authorP);
-                });
-                // Описание
-                descriptionHtml.textContent = bookInfo.description;
-                // Список отзывов
-                for (const review of bookInfo.reviews) {
-                    const bufferDiv = document.createElement('reviewDiv.html');
-                    const response = await fetch("reviewDiv.html");
-                    bufferDiv.innerHTML = await response.text();
-                    const reviewDiv = bufferDiv.getElementById("reviewDiv");
-
-                    // На будущее сохраню id
-                    reviewDiv.setAttribute("reviewId", review.id);
-
-                    // Выделяем поля в шаблоне
-                    let dateHtml = reviewDiv.getElementById("date");
-                    let userNameHtml = reviewDiv.getElementById("user-name");
-                    let markHtml = reviewDiv.getElementById("mark");
-                    let textHtml = reviewDiv.getElementById("text");
-
-                    // Заполняем поля
-                    dateHtml = review.date;
-                    userNameHtml = review.user.name;
-                    markHtml = review.mark;
-                    textHtml = review.text;
-
-                    // Сохраним id пользователя на будущее
-                    userNameHtml.setAttribute("userId", review.user.id);
-
-                    // Закидываем готовый отзыв в список
-                    reviewsDivHtml.append(bufferDiv);
-                }
-                // Кнопка для чтения
-                readButton.setAttribute("bookId", bookId);
-                readButton.onclick = async function (event) {
-                    const bookId = readButton.getAttribute("bookId");
-                    event.preventDefault();
-                    const epubUrl = await fetch(`getBookEpubFile.epub?${encodeURIComponent(bookId)}`);
-                    openReader(epubUrl);
-                }
-
-                // Показываем модальное окно
-                modalWindow.display = "block";
-            };
-
-            // Закидываем кнопку
-            bookDiv.append(readButton);
-
-            // Закидываем готовое поле книги
-            resultsDiv.append(bookDiv);
-        });
+        data.forEach(book => resultsDiv.append(createBookDiv(book)));
     } else {
         resultsDiv.innerHTML = '<p>Книги не найдены.</p>';
     }
-});
+}
+
+function createBookDiv(book) {
+    // Отображение результатов
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book-div';
+    const authorsStr = book.authors
+        .map(author => author.name)
+        .join(', ');
+    bookDiv.innerHTML = `<p> <strong>${book.name}</strong> (${authorsStr})</p> `;
+
+    // Добавление кнопки для перехода к описанию книги
+    const readButton = document.createElement('button');
+    readButton.innerText = 'Инфо';
+    readButton.onclick = (event) => showBookInfo(event, book.id);
+
+    // Закидываем кнопку
+    bookDiv.append(readButton);
+
+    return bookDiv;
+}
+
+async function showBookInfo(event, bookId) {
+    event.preventDefault();
+
+    // Запрашиваем информацию о книге у сервера
+    const bookInfoResponse = await fetch(`/getBookInfo?id=${encodeURIComponent(bookId)}`);
+    const bookInfo = await bookInfoResponse.json();
+
+    // Загружаем шаблон book-info-content
+    const response = await fetch("book-info-content.html");
+    contentPlace.innerHTML = await response.text();
+    const bookInfoContent = contentPlace.querySelector("#book-info-content");
+
+    // Выделяем нужные поля в шаблоне
+    const nameHtml = bookInfoContent.querySelector("#name");
+    const authorsDivHtml = bookInfoContent.querySelector("#authors");
+    const descriptionHtml = bookInfoContent.querySelector("#description");
+    const reviewsDivHtml = bookInfoContent.querySelector("#reviews");
+    const readButton = bookInfoContent.querySelector("#read-button");
+
+    // Обновление содержимого модального окна
+    // Название книги
+    nameHtml.textContent = bookInfo.name;
+    // Список авторов
+    bookInfo.authors.forEach(author => authorsDivHtml.append(createAuthorP(author)));
+    // Описание
+    descriptionHtml.textContent = bookInfo.description;
+    // Список отзывов
+    bookInfo.reviews.forEach(review => reviewsDivHtml.append(createReviewDiv(review)));
+    // Кнопка для чтения
+    readButton.onclick = (event) => readBook(event, bookId);
+
+    // Показываем модальное окно
+    modalWindow.display = "block";
+}
+
+function createAuthorP(author) {
+    const authorP = document.createElement('p');
+    authorP.className = "book-info-content__text";
+
+    authorP.innerHTML = author.name;
+
+    // На будущее сохранил id автора
+    authorP.setAttribute("authorId", author.id);
+
+    return authorP;
+}
+
+async function createReviewDiv(review) {
+    // Временный объект, чтобы получить в использование объект из reviewDiv.html
+    const bufferDiv = document.createElement('div');
+
+    // Лутаем html код нашего reviewDiv
+    const response = await fetch("reviewDiv.html");
+    bufferDiv.innerHTML = await response.text();
+
+    // Достаём наш объект из буфера
+    const reviewDiv = bufferDiv.querySelector("#reviewDiv");
+
+    // Выделяем поля в шаблоне
+    const dateHtml = reviewDiv.querySelector("#date");
+    const userNameHtml = reviewDiv.querySelector("#user-name");
+    const markHtml = reviewDiv.querySelector("#mark");
+    const textHtml = reviewDiv.querySelector("#text");
+
+
+    // Заполняем поля
+    dateHtml.textContent = review.date;
+    userNameHtml.textContent = review.user.name;
+    markHtml.textContent = review.mark;
+    textHtml.textContent = review.text;
+
+
+    // На будущее сохраню id отзыва
+    reviewDiv.setAttribute("reviewId", review.id);
+
+    // На будущее сохраню id пользователя
+    userNameHtml.setAttribute("userId", review.user.id);
+
+    return reviewDiv;
+}
+
+async function readBook(event, bookId) {
+    event.preventDefault();
+
+    const epubUrl = await fetch(`getBookEpubFile.epub?${encodeURIComponent(bookId)}`);
+    openReader(epubUrl);
+}
 
 // Закрытие модального окна
 function closeModal() {
-    document.getElementById("modal-window").style.display = "none";
+    modalWindow.style.display = "none";
 }
 
 function openReader(epubUrl) {
