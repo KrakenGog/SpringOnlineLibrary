a4261c9136d1bc38132fcabb9f484133b71d93f3
SanchukS <SanchukS2005@gmail.com>
2024-12-23 17:58:48 +0300

    add button for adding review

--- src/main/java/com/onlib/core/controller/ReviewController.java
+++ src/main/java/com/onlib/core/controller/ReviewController.java
@@ -1,19 +1,27 @@
 package com.onlib.core.controller;
 
 import com.onlib.core.dto.ReviewDto;
+import com.onlib.core.model.LibraryUser;
+import com.onlib.core.repository.UserRepository;
 import com.onlib.core.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ReviewController {
     @Autowired
     ReviewService reviewService;
+
+    @Autowired
+    UserRepository userRepository;
+    private UserController userController;
 
     @GetMapping("/getBookReviews")
     public ResponseEntity<List<ReviewDto>> getBookReviews(
@@ -31,13 +39,19 @@ public class ReviewController {
 
     @PostMapping("/addBookReview")
     public ResponseEntity<Object> addBookReview(
-            @RequestParam Long userId,
             @RequestParam Long bookId,
             @RequestParam String text,
-            @RequestParam Long markValue
+            @RequestParam Long rating
     ) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<LibraryUser> user = userRepository.findByName(userName);
+        if (user.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Long userId = user.get().getId();
+
         try {
-            reviewService.addReview(userId, bookId, text, markValue);
+            reviewService.addReview(userId, bookId, text, rating);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (NotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
